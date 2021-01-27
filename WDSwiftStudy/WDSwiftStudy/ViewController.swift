03a5fd5f85f10382bc74def7d8f6a850a4600ccb
weidong <jiangweidong@hetao101.com>
2021-01-27 18:25:34 +0800

    运算符的设置

--- WDSwiftStudy/WDSwiftStudy/ViewController.swift
+++ WDSwiftStudy/WDSwiftStudy/ViewController.swift
@@ -14,8 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 0.5)
         
+        let a = 20
+        let b = 33.7
+        let c = a + Int(b)
+        let d = Double(a) + b
         
-        
+        print("c = \(c)")
+        print("d = \(d)")
+
     }
 
 
