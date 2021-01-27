0875d6d0197c9b76d7f63d5f564fc77afe5126af
weidong <jiangweidong@hetao101.com>
2021-01-27 16:14:13 +0800

    初始化swift工程

---
+++ WDSwiftStudy/WDSwiftStudy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WDSwiftStudy
+//
+//  Created by 伟东 on 2021/1/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 0.5)
+        
+        
+        
+    }
+
+
+}
+
